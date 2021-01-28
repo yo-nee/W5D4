4ec1eb46825687d01f9b84a8b6f4ac8c61d14e59
Ray <raysidarta@gmail.com>
2021-01-28 14:45:04 -0500

    Completed Course associations for enrollments and users

--- associations_excercise/app/models/course.rb
+++ associations_excercise/app/models/course.rb
@@ -10,4 +10,27 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :enrollment_id,
+        class_name: :Enrollment
+
+    has_many  :users,
+        primary_key: :id,
+        foreign_key: :enrollment_id,
+        class_name: :User
+
+
+    validates :name, null: false
+    validates :prereq_id, null: false
+    validates :instructor_id, null: false
+
+
+
+
+
+
+
 end
