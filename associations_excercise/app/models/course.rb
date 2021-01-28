8c4cf000f7e5c2f1b905e19e8e6f950479642b41
yonatan zonszein <yonatanzonszein@gmail.com>
2021-01-28 16:49:48 -0500

    Completed associations excercise

--- associations_excercise/app/models/course.rb
+++ associations_excercise/app/models/course.rb
@@ -16,13 +16,24 @@ class Course < ApplicationRecord
         foreign_key: :enrollment_id,
         class_name: :Enrollment
 
-    has_many  :users,
+    has_many  :enrolled_students,
         through: :enrollments,
         source: :user
 
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+  
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional:true
+
 
     validates :name, presence: true
-    validates :prereq_id, presence: true
+    # validates :prereq_id, presence: true
     validates :instructor_id, presence: true
 
 
